0f14760e558581cf7b7348708c1ee100f7b401c0
gaddamanand <gaddamanand13@gmail.com>
2021-12-02 17:34:19 +0700

    commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Contact</h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Asperiores, quibusdam.
+            </p>
+        </div>
+        )
+    }
+    }
+
+    export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            
+         }
+    }
+
+    render() { 
+        return ( 
+            <>
+                <header >
+                    <div>
+                        <h1>Home</h1>
+                        
+                    </div>
+                </header>
+            </>
+         );
+    }
+}
+ 
+export default Home;
